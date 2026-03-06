1f1a6cb15549ebabfa5512906dc6584c8cac0344
talatuccar <talatuccar@outlook.com>
2026-03-06 07:05:51 +0300

    feat: implement automatic scene transition from Bootstrap to Menu
    rename scenes with numeric prefix and update build settings

--- Assets/_Project/Core/ProjectBootstrap.cs
+++ Assets/_Project/Core/ProjectBootstrap.cs
@@ -1,6 +1,8 @@
-using UnityEngine;
-using Project.Infrastructure.DependencyInjection;
+using Cysharp.Threading.Tasks;
 using Project.Core.EventBus;
+using Project.Infrastructure.DependencyInjection;
+using Project.Infrastructure.Services;
+using UnityEngine;
 
 public class ProjectBootstrap : MonoBehaviour
 {
@@ -18,9 +20,20 @@ public class ProjectBootstrap : MonoBehaviour
     private void InitializeServices()
     {
         var eventBus = new EventBus();
-
         Container.Register<IEventBus>(eventBus);
 
+        var sceneLoader = new SceneLoader();
+        Container.Register<ISceneLoader>(sceneLoader);
+
         Debug.Log("Services initialized");
+    }
+
+    private async void Start()
+    {
+
+        var sceneLoader = Container.Get<ISceneLoader>();
+       
+        await UniTask.Delay(500);
+        await sceneLoader.LoadScene(SceneNames.Menu);
     }
 }
---
+++ Assets/_Project/Core/SceneNames.cs
@@ -0,0 +1,6 @@
+public static class SceneNames
+{
+    public const string Bootstrap = "00_Bootstrap";
+    public const string Menu = "01_MainMenu";
+    public const string Gameplay = "02_Gameplay";
+}
